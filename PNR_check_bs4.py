adadacb9c5366005adaa54453159aa57834bb1fd
ilangovenac <acilangoven@gmail.com>
2021-01-20 05:46:29 +0530

    first commit

---
+++ PNR_check_bs4.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+pnr=input('Enter PNR number: ')
+url='https://www.makemytrip.com/railways/pnrsearch/?pnr='+'pnr'
+
+src=requests.get(url)
+soup=BeautifulSoup(src.content,'html.parser')
+
+result=soup.find("p", attrs={"class":"font16 lightGreyText appendBottom30"})
+#result=soup.find("data-cy"="pnrFlushedTxt")
+
+result_final=result.string
+
+print(result_final)
+
+#result_final.strip()
+#print(result_final)
